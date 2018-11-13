b95e9124856b171953181e621794296241bdc476
SmilingWolf <lupo996@gmail.com>
2018-11-13 09:28:52 +0100

    Add some Fate/SN stuff

---
+++ ImageMergePlanes/README.md
@@ -0,0 +1,6 @@
+An idea by Nintendo Maniac 64 for the "Ultimate" Fate/Stay Night eng patch
+Scripts to merge the Y level of the Android port WebP image with the upsampled PC PNG Chroma correspective
+
+For more infos: http://forums.nrvnqsr.com/showthread.php/4745-Fate-Stay-Night-Realta-Nua-PC-version-Mirror-Moon-TL-insertion-project?p=2908509&viewfull=1#post2908509
+
+Use Pillow and Multiprocessing modules
---
+++ ImageMergePlanes/mergelevels.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import os, sys, glob
+from PIL import Image
+import numpy
+
+def rgb2ycbcr(im):
+	xform = numpy.array([[.299, .587, .114], [-.168736, -.331264, .5], [.5, -.418688, -.081312]])
+	ycbcr = im.dot(xform.T)
+	ycbcr[:,:,[1,2]] += 128
+	return numpy.float64(ycbcr)
+
+def ycbcr2rgb(im):
+	xform = numpy.array([[1, 0, 1.402], [1, -0.344136, -.714136], [1, 1.772, 0]])
+	rgb = im.astype(numpy.float64)
+	rgb[:,:,[1,2]] -= 128
+	rgb = rgb.dot(xform.T)
+	numpy.clip(rgb, 0, 255, rgb)
+	return numpy.uint8(rgb)
+
+imagesList = glob.glob(u'android/*.webp')
+
+for file in imagesList:
+	fileName = os.path.basename(file)
+	fileName, fileExt = os.path.splitext(fileName)
+	YSource = Image.open(file)
+	imSizes = YSource.size
+
+	YPlane = numpy.array(YSource.getdata(), numpy.float64).reshape((imSizes[1], imSizes[0], 4))
+	YPlane = YPlane[:,:,[0, 1, 2]]
+	YPlane = rgb2ycbcr(YPlane)[:,:,0]
+
+	CbCrSource = Image.open(u'waifu2x/%s.png' % fileName)
+	CbCrSource = CbCrSource.resize((imSizes[0], imSizes[1]), Image.LANCZOS)
+
+	if CbCrSource.mode == 'RGBA':
+		CbCrPlane = numpy.array(CbCrSource.getdata(), numpy.float64).reshape((imSizes[1], imSizes[0], 4))
+		AlphaPlane = numpy.uint8(CbCrPlane[:,:,3])
+		CbCrPlane = CbCrPlane[:,:,[0, 1, 2]]
+	else:
+		CbCrPlane = numpy.array(CbCrSource.getdata(), numpy.float64).reshape((imSizes[1], imSizes[0], 3))
+		AlphaPlane = None
+
+	CbCrPlane = rgb2ycbcr(CbCrPlane)
+	CbPlane = CbCrPlane[:,:,1]
+	CrPlane = CbCrPlane[:,:,2]
+
+	YCArray = numpy.zeros((imSizes[1], imSizes[0], 3), numpy.float64)
+	YCArray[..., 0] = YPlane
+	YCArray[..., 1] = CbPlane
+	YCArray[..., 2] = CrPlane
+
+	RGBArray = ycbcr2rgb(YCArray)
+	if AlphaPlane is None:
+		saveMode = 'RGB'
+	else:
+		RGBArray = numpy.dstack((RGBArray, AlphaPlane))
+		saveMode = 'RGBA'
+
+	imOutput = Image.fromarray(RGBArray, saveMode)
+	imOutput.save(u'merged/%s.webp' % fileName, 'WebP', lossless=True, quality=100, method=6)
