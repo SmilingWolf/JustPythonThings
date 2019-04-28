e8dd82a62cb3cac835aeb1737df6aca54f550a0d
SmilingWolf <lupo996@gmail.com>
2019-04-28 08:48:23 +0200

    New model
    - different loss function: logcosh
    - improvements on all 3 datasets

--- ML-IQA/IQMLP.test.py
+++ ML-IQA/IQMLP.test.py
@@ -7,24 +7,25 @@ from sklearn.externals import joblib
 
 from keras.models import load_model
 
-data = pd.read_csv('scores.csv')
+for database in ['CSIQ', 'LIVE2', 'TID2013']:
+	data = pd.read_csv('scores-%s.csv' % database)
 
-for modelName in sys.argv[1:]:
-	scaler = joblib.load('%s.wgt' % modelName)
-	dataMod = pd.DataFrame(data=scaler.transform(data), columns=data.columns, index=data.index)
+	for modelName in sys.argv[1:]:
+		scaler = joblib.load('%s.wgt' % modelName)
+		dataMod = pd.DataFrame(data=scaler.transform(data), columns=data.columns, index=data.index)
 
-	X = dataMod.filter(items=['PSNRY', 'PSNRCb', 'PSNRCr', 'SSIMY', 'SSIMCb', 'SSIMCr'], axis=1).values
+		X = dataMod.filter(items=['PSNRY', 'PSNRCb', 'PSNRCr', 'SSIMY', 'SSIMCb', 'SSIMCr'], axis=1).values
 
-	model = load_model('%s.h5' % modelName)
-	model.summary()
+		model = load_model('%s.h5' % modelName)
+		model.summary()
 
-	predicted = model.predict(X)
+		predicted = model.predict(X)
 
-	invScaler = StandardScaler()
-	invScaler.scale_, invScaler.mean_, invScaler.var_ = scaler.scale_[0], scaler.mean_[0], scaler.var_[0]
+		invScaler = StandardScaler()
+		invScaler.scale_, invScaler.mean_, invScaler.var_ = scaler.scale_[0], scaler.mean_[0], scaler.var_[0]
 
-	results = invScaler.inverse_transform(predicted)
+		results = invScaler.inverse_transform(predicted)
 
-	with open('%s.txt' % modelName, 'w') as hFile:
-		for mos in results:
-			hFile.write('%.05f\n' % mos[0])
+		with open('%s-%s.txt' % (modelName, database), 'w') as hFile:
+			for mos in results:
+				hFile.write('%.05f\n' % mos[0])
--- ML-IQA/IQMLP.train.py
+++ ML-IQA/IQMLP.train.py
@@ -11,26 +11,19 @@ from keras.optimizers import SGD
 
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 
-data = pd.read_csv('scores.csv')
-valData = pd.read_csv('val_scores.csv')
+data = pd.read_csv('scores-CSIQ.csv')
+data = data.append(pd.read_csv('scores-LIVE2.csv'))
+data = data.append(pd.read_csv('scores-TID2013.csv'))
 
 scaler = StandardScaler()
 data = pd.DataFrame(data=scaler.fit_transform(data), columns=data.columns, index=data.index)
 joblib.dump(scaler, 'scaler.wgt')
 
-valData['MOS'] = valData['MOS'] * (9/100.0)
-valData = pd.DataFrame(data=scaler.transform(valData), columns=valData.columns, index=valData.index)
-
 Y = data.filter(items=['MOS'], axis=1).values
 X = data.filter(items=['PSNRY', 'PSNRCb', 'PSNRCr', 'SSIMY', 'SSIMCb', 'SSIMCr'], axis=1).values
 
-valY = valData.filter(items=['MOS'], axis=1).values
-valX = valData.filter(items=['PSNRY', 'PSNRCb', 'PSNRCr', 'SSIMY', 'SSIMCb', 'SSIMCr'], axis=1).values
-
 i = Input(shape=(6, ))
-x = Dense(7, kernel_initializer='lecun_normal')(i)
-x = Activation('selu')(x)
-x = Dense(5, kernel_initializer='lecun_normal')(x)
+x = Dense(5, kernel_initializer='lecun_normal')(i)
 x = Activation('selu')(x)
 x = Dense(3, kernel_initializer='lecun_normal')(x)
 x = Activation('selu')(x)
@@ -41,7 +34,7 @@ model = Model(inputs=i, outputs=o)
 opt = SGD(lr=0.01, momentum=0.9, nesterov=True)
 model.compile(optimizer=opt, loss='logcosh')
 
-mc = ModelCheckpoint('models/IQMLP-{epoch:02d}-{val_loss:.4f}.h5', save_best_only=True, monitor='val_loss', mode='min')
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', verbose=1, factor=0.5, patience=10, min_lr=0.00001)
+mc = ModelCheckpoint('models/IQMLP-{epoch:02d}-{loss:.4f}.h5', save_best_only=True, monitor='loss', mode='min')
+reduce_lr = ReduceLROnPlateau(monitor='loss', verbose=1, factor=0.5, patience=10, min_lr=0.00001)
 
-model.fit(x=X, y=Y, verbose=1, epochs=200, validation_data=(valX, valY), callbacks=[reduce_lr, mc])
+model.fit(x=X, y=Y, verbose=1, epochs=200, validation_split=0.0, callbacks=[reduce_lr, mc])
